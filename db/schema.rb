240471f83b74ff8f98c41e8a5ba93013f5ce5543
钱飞宏 <feihongqian1122@gmail.com>
2020-11-08 22:12:58 -0800

    fix question html

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201106111715) do
+ActiveRecord::Schema.define(version: 20201109155309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20201106111715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "questions", force: :cascade do |t|
     t.integer "payment_id"
     t.integer "user_id"
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 20201106111715) do
     t.datetime "updated_at", null: false
     t.integer "house_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
