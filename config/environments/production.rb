b41d4a959ee21fe2059646aca87a41167b8ed61f
mengyushi <meng.yushi@yahoo.com>
2020-12-02 13:52:47 -0800

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   #config.action_mailer.default_url_options = { :host => 'room-ies.herokuapp.com', :protocol => 'https'}
 
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   # config.action_mailer.delivery_method = :smtp
   # host = 'dms297p.herokuapp.com'
   # config.action_mailer.default_url_options = { host: host }
@@ -101,8 +101,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "dms297p@gmail.com",
-    :password             => "dms297pgmail",
+    :user_name            => "297dms@gmail.com",
+    :password             => "297dmsgmail",
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
